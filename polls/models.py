50a78167a5ecf585a75efa4ec94725f852f2e0f0
sicilly <634068240@qq.com>
2020-01-03 22:49:54 +0800

    模型与后台

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,29 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Question(models.Model):
+    # 问题
+    question_text = models.CharField(max_length=200)
+    # 发布日期
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    # 外键：每一个Choice关联到一个对应的Question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # 选项描述
+    choice_text = models.CharField(max_length=200)
+    # 投票数（默认值设为0）
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
